6bb3819056f66ae4228e8fe014647594c531d850
Andres Hidalgo <andres.hidalgo@gmail.com>
2013-07-09 22:49:35 +0000

    Final changes

--- web.js
+++ web.js
@@ -1,33 +1,20 @@
 var express = require('express');
 var fs = require('fs');
-
 var app = express.createServer(express.logger());
-
 var filename = "index.html";
 
 app.get('/', function(request, response) {
 
-fs.readFile(filename, function(err, data) {
-
-        if (err instanceof Error) {
-                console.log("Error opening + " + filename);
-		response.send("Can't find index.html<br />\n");  
-      } else {
-                msg = new Buffer(data);
-                /* console.log(data.toString()); */
-		 //response.send(msg); 
-
-
-	response.setHeader("Content-Type", "text/html");
-
-	response.send(msg);
-
-
-        }
-});
-
- //response.send('Hello World 2!'); 
-
+	fs.readFile(filename, function(err, data) {
+        	if (err instanceof Error) {
+	                console.log("Error opening + " + filename);
+			response.send("Can't find index.html<br />\n");  
+		} else {
+        	        msg = new Buffer(data);
+			response.setHeader("Content-Type", "text/html");
+			response.send(msg);
+        	}
+	});
 });
 
 var port = process.env.PORT || 5000;
