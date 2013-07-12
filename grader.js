5f96c15f3ffef8c3cd19cecacc9cc50cd1570849
Andres Hidalgo <andres.hidalgo@gmail.com>
2013-07-12 03:33:20 +0000

    Added last changes to grader.js, checks.js and index.html

--- grader.js
+++ grader.js
@@ -22,10 +22,14 @@ References:
 */
 
 var fs = require('fs');
+var http = require('http');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.google.com";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +39,21 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var urlExists = function(address){
+	rest.get(address).on('complete', function(result) {
+	  if (result instanceof Error) {
+	    console.log('Error: ' + result.message);
+		process.exit(1);
+	  } else {
+		fs.writeFile('tmp.html', result, function (err) {
+		  if (err) throw err;
+			return 'tmp.html';
+		});
+  	  }
+	});
+}
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,10 +84,25 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_address>', 'URL address', clone(urlExists))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	var file = "";	
+
+	if(program.url) {
+		file = "tmp.html";
+	} else {
+		file = program.file;
+	}
+		setTimeout(function(){
+			var checkJson = checkHtmlFile(file, program.checks);
+			var outJson = JSON.stringify(checkJson, null, 4);
+			console.log(outJson);
+			if(file == "tmp.html") {
+				fs.unlink('tmp.html');
+			}
+
+		},1000);
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
